a9ba2781bdb9c0255b172c0e1efd59142ae5e40b
Rail Gonzáles aldana <railgonzalesaldana603@gmail.com>
2025-11-12 00:20:31 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,46 +1,33 @@
 import './App.scss';
-import { Routes, Route ,useLocation} from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 
 import Home from './containers/home';
 import About from './containers/about';
-import Resume from './containers/resume/index'; 
+import Resume from './containers/resume';
 import Skills from './containers/skills';
 import Portfolio from './containers/portfolio';
 import Contact from './containers/contact';
 import Navbar from './components/Navbar';
-
-import ParticlesComponent from './utils.js/particles';
+import ParticlesComponent from './components/particles'; 
 
 function App() {
-
   const location = useLocation();
-  console.log(location);
-
-  const renderParticleJsInHome=location.pathname ==="/"; 
-
+  const renderParticleJsInHome = location.pathname === "/";
 
   return (
     <div className="App">
-      {/* Particles.js */}
-
-      {
-        renderParticleJsInHome &&
-        <ParticlesComponent id="particles" />
-      }
-
-
-      {/* Navbar */}
+      {renderParticleJsInHome && <ParticlesComponent id="particles" />}
       <Navbar />
-      {/* Main page content */}
-      <div className='App_main-page-content'></div>
-      <Routes>
-        <Route index path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/resume" element={<Resume />} />
-        <Route path="/skills" element={<Skills />} />
-        <Route path="/portfolio" element={<Portfolio />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+      <div className='App_main-page-content'>
+        <Routes>
+          <Route index path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
     </div>
   );
 }
